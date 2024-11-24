42efd9394ff2594a6f9e19c18a381184ccec023f
Rem Baumann <baumann.rem@gmail.com>
2024-11-24 18:03:07 +0000

    add instance_type variable to variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,4 +3,8 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
-variable "instance_type" {}
+variable "instance_type" {
+  description = "EC2 instance type to deploy on"
+  type        = string
+  default     = "t2.micro"
+}
